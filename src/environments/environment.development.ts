a5d4dcd4cce54bed795517c3a4c6a8853a25c656
novelkhan <novel4004@gmail.com>
2024-08-19 03:45:23 +0600

    API path is updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // personApiBaseUrl: 'https://ng.somee.com/api'
-    personApiBaseUrl: 'https://localhost:7258/api'
+    personApiBaseUrl: 'https://ng.somee.com/api'
+    // personApiBaseUrl: 'https://localhost:7258/api'
 };
