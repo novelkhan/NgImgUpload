e32f32dffde2c3dc242d24a9f2b015d5506c8d9f
novelkhan <novel4004@gmail.com>
2025-02-02 04:14:44 +0600

    add component is created inside Item module in it's dedicated components folder using 'ng g c add --skip-tests' command

--- src/app/modules/item/item.module.ts
+++ src/app/modules/item/item.module.ts
@@ -5,12 +5,14 @@ import { ItemRoutingModule } from './item-routing.module';
 import { ItemComponent } from './components/item/item.component';
 import { AddItemComponent } from './components/add-item/add-item.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AddComponent } from './components/add/add.component';
 
 
 @NgModule({
   declarations: [
     ItemComponent,
-    AddItemComponent
+    AddItemComponent,
+    AddComponent
   ],
   imports: [
     CommonModule,
