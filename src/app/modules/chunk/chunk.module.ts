2040a09d8d91d4acd1ccf4d722bd376590e25fdc
Novel Khan <novel4004@gmail.com>
2026-04-25 01:41:02 +0600

    add-chunked component is created inside chunk module in it's dedicated components folder using 'ng g c add-chunked --skip-tests' command

--- src/app/modules/chunk/chunk.module.ts
+++ src/app/modules/chunk/chunk.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ChunkRoutingModule } from './chunk-routing.module';
+import { AddChunkedComponent } from './components/add-chunked/add-chunked.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddChunkedComponent
+  ],
   imports: [
     CommonModule,
     ChunkRoutingModule
---
+++ src/app/modules/chunk/components/add-chunked/add-chunked.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-chunked',
+  templateUrl: './add-chunked.component.html',
+  styleUrl: './add-chunked.component.scss'
+})
+export class AddChunkedComponent {
+
+}
